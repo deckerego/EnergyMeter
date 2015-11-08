f20f3b697abae9faeadade212e3bf1576f496a81
John Ellis <deckerego@gmail.com>
2015-11-07 21:38:35 -0500

    Actual working version
    Actually provides amperage correctly by calculating RMS

--- EnergyMeter/EnergyMeter.ino
+++ EnergyMeter/EnergyMeter.ino
@@ -1,8 +1,15 @@
+// Note that I'm assuming we're using a 5V Adafruit Trinket and
+// a SCT-013-000 current transformer sensor below. Values are
+// hard-coded to cut down on sketch size.
+
 #include "TrinketFakeUsbSerial.h"
+
+double offset = 512.0; // Assuming a 10-bit DAC, so 1024 / 2 in base10
 
 void setup()
 {
   TFUSerial.begin();
+
   pinMode(1, OUTPUT);
   pinMode(2, INPUT);
 }
@@ -11,8 +18,22 @@ void loop()
 {
   TFUSerial.task();
 
-  int calibrationValue = 508;
-  analogWrite(1, (analogRead(1) - calibrationValue) * 2);
+  double sum = 0.0;
+  for (unsigned int n = 0; n < 1200; n++) {
+    int voltage = analogRead(1);
+    offset = (offset + (voltage - offset) / 1024); // Also assuming a 10-bit DAC
+    double filtered = voltage - offset;
+    sum += filtered * filtered;
+  }
 
-  TFUSerial.println(analogRead(1) - calibrationValue);
+  // Ratio used for conversion from RMS is: Ic * (V / Rdac)
+  // Where Ic is the current calibration coefficient,
+  // V is the accepted voltage of the input pin, and
+  // Rdac is the resolution of the digital/analog converter.
+  // For the current calibration coefficient, I'm assuming
+  // a 33 Ω ±1% burden resistor over a current transformer that
+  // transforms 100A to 50mA, or: (100A / 0.05A) / 33 Ω = 60.6060...
+  // I'm also assuming a 5V Trinket with a 10-bit DAC, and so:
+  // Ic * (5V / 10 bits) = 60.6060 * (5.0 / 1024) = 0.29592803030...
+  TFUSerial.println(0.2959280303030 * sqrt(sum / 1200));
 }
--- README.md
+++ README.md
@@ -5,7 +5,7 @@ An Arduino energy monitor, constructed for a Trinket
 
 See also:
 
-http://openenergymonitor.org/emon/node/58
+http://openenergymonitor.org/emon/buildingblocks/how-to-build-an-arduino-energy-monitor-measuring-current-only
 
 As implemented by:
 
@@ -13,3 +13,6 @@ http://www.homautomation.org/2013/09/17/current-monitoring-with-non-invasive-sen
 
 https://www.hackster.io/yleguesse/spark-non-invasive-smart-electricity-meter-670862
 
+Calibration is documented at:
+
+http://openenergymonitor.org/emon/buildingblocks/calibration
